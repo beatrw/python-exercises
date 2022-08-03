3e7fb62a670da229dd85064664c05065e1347e86
Beatriz Aquino <aquinobeattriz@gmail.com>
2022-08-02 23:04:31 -0300

    Update ex003.py

---
+++ ex003.py
@@ -0,0 +1,15 @@
+#Operações aritméticas
+print(5 + 2) #adição
+print(5 - 2) #subtração
+print(5 * 2) #multipllicação
+print(5 / 2) #divisão
+print(5 ** 2) #potenciação
+print(5 // 2) #divisão inteira
+print(5 % 2) #mod/resto da divisão
+
+#Ordem de precedência de operações matemáticas:
+# 1: ()
+# 2: **
+# 3: * / // %
+# 4: + -
+
